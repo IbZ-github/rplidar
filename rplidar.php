43aabaa1a488909823baba7d7edd249b6c5946d0
Evgeny Kushmanov <ibz@ibz.ru>
2022-03-31 15:51:41 +0300

    added getHealth

--- rplidar.php
+++ rplidar.php
@@ -58,6 +58,14 @@ class Lidar
         return $this->resource->readPort($count);
     }
 
+    /**
+     * Read descriptor of responsed data
+     *
+     * @return $array( string START_FLAG,     must be always in hex 0xA55A
+     *                 long DSIZE,            size next 30bit in response
+     *                 byte SEND_MODE,        next 2bit in response - mode: 0 - single, 1 - multi response, 2 and 3 - reserved for future 
+     *                 byte DTYPE )           descriptor type
+     */
     private function read_descriptor()
     {
         $descriptor = $this->read(DESCRIPTOR_LEN);
@@ -82,11 +90,11 @@ class Lidar
      * @param int $mode  Cut mode, either CUT_FULL or CUT_PARTIAL
      * @param int $lines Number of lines to feed
      *
-     * @return $array( int MODEL,
-     *                 int FW_MAJOR,
-     *                 int FW_MINOR,
-     *                 int HARDWARE
-     *                 string(15) SERIALNUMBER )
+     * @return $array( byte MODEL,            model, 24 - A1, 97 - S1
+     *                 byte FW_MAJOR,         firmware major version
+     *                 byte FW_MINOR,         firmware minor version
+     *                 byte HARDWARE,         hardware, 7 - A1, 18 - S1
+     *                 string SERIALNUMBER )
      */
     public function getInfo()
     {
@@ -105,10 +113,38 @@ class Lidar
             throw new Exception('Wrong response data type');
         }
         $rawdata = $this->read(GET_INFO_LEN);
-        $data = unpack("CMODEL/CFW_MINOR/CFW_MAJOR/CHARDWARE/A15SERIALNUMBER",$rawdata);
+        $data = unpack("CMODEL/CFW_MINOR/CFW_MAJOR/CHARDWARE/A16SERIALNUMBER",$rawdata);
         $data["SERIALNUMBER"] = bin2hex($data["SERIALNUMBER"]);
         return $data;
     }
+
+    /**
+     * Get health status from Lidar
+     *
+     * @return $array( byte STATUS,   0 - Good, 1 - Warning, 2 - Error
+     *                 int ERROR_CODE )
+     */
+    public function getHealth()
+    {
+        $this->write(SYNC_S_BYTE . GET_HEALTH_BYTE);
+        $descriptor = $this->read_descriptor();
+        if($descriptor["DSIZE"] != GET_HEALTH_LEN)
+        {
+            throw new Exception('Wrong get_health reply length');
+        }
+        if($descriptor["SEND_MODE"] != SEND_MODE_SINGLE)
+        {
+            throw new Exception('Not a single response mode');
+        }
+        if($descriptor["DTYPE"] != GET_HEALTH_TYPE)
+        {
+            throw new Exception('Wrong response data type');
+        }
+        $rawdata = $this->read(GET_HEALTH_LEN);
+        $data = unpack("CSTATUS/SERROR_CODE",$rawdata);
+        return $data;
+    }
+
 }
 
 
